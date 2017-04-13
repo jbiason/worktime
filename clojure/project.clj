3bfae48bd5f5b25f906f4b715d578efb6e078e78
Julio Biason <julioandre@cwi.com.br>
2017-04-13 17:26:18 -0300

    just process the times into datetimes, nothing more

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,9 +1,9 @@
 (defproject worktime "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple time tracking app"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :license {:name "GPLv3"
+            :url "https://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"] [clj-time "0.8.0"]]
   :main ^:skip-aot worktime.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- clojure/src/worktime/core.clj
+++ clojure/src/worktime/core.clj
@@ -1,7 +1,39 @@
 (ns worktime.core
-  (:gen-class))
+  (:gen-class)
+  (require [clj-time.core :as t])
+  (require [clj-time.format :as f]))
+
+
+(def output-formatter (f/formatter "HH:mm"))
+
+
+(defn break-time-string
+  "Break strings into their time components"
+  [clock]
+  (map #(Integer/parseInt %) (clojure.string/split clock #":")))
+  ; (fn [x] (Integer/parseInt x))
+
+
+(defn conv-time
+  "Convert times into a full datetime"
+  [times]
+  (map #(apply t/today-at (break-time-string %)) times))
+
+
+(defn build-turn
+  "Build a turn of work"
+  [turn-list worked-time time-list]
+  (let [turn-times (take 2 time-list)]
+    (build-turn turn-list worked-time (drop 2 time-list)))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Do the whole thing, getting values from the command line and
+   building the turns with either the values or guessing the next
+   ones."
   [& args]
-  (println "Hello, World!"))
+  (->> args
+       (conv-time)
+       (map #(f/unparse output-formatter %))
+       (clojure.string/join " ")
+       (println)))
